3e656c0d88834061f2d02449e16e2a8d0a390335
Alexey Shevchenko <otetz@me.com>
2017-12-07 22:17:19 +0200

    Setup, reqs and tox.

--- requests_proxy_adapter/__init__.py
+++ requests_proxy_adapter/__init__.py
@@ -2,12 +2,13 @@
 
 """Top-level package for Requests Proxy Adapter."""
 
-__author__ = """Alexey Shevchenko"""
-__email__ = 'otetz@me.com'
-__version__ = '0.1.0'
-
 import logging
 
 from .api import PrivoxyAdapter, RetryPrivoxyAdapter  # noqa: F401
+from .version import __version__  # noqa: F401
 
 logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+__author__ = "Alexey Shevchenko"
+__email__ = 'otetz@me.com'
+__copyright__ = "Copyright 2017, Alexey Shevchenko"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Unit test package for requests_proxy_adapter."""
+
 import os
 
 PROXY_HOST = os.getenv('PROXY_HOST')
 URL_HTTP = 'http://httpbin.org/ip'
+
+assert PROXY_HOST
